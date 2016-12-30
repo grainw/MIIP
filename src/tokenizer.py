fa606de7ff6944d3717c7002708c521d3d0dd1d2
Liu Ming <miliu@statoil.com>
2016-12-30 10:40:59 +0800

    add argument parser.

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -7,19 +7,26 @@ from LoggerFacotory import *
 import jieba.posseg as pseg
 import jieba
 import lda
+import argparse
 #enable parallel for jieba
 jieba.enable_parallel()
 
 
-data = FileUtils('../train/dxy1.json', FileType.JSON).doRead()
-stop_words = FileUtils('../train/stopwords1.txt', FileType.TEXT, ["words"]).doRead()
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', "--sourcefile", help="number of threads for running", type=str)
+parser.add_argument('-s', '--stopwords', help='stop words list', type=str)
+parser.add_argument('-u', '--userdict', help='specified user dict', type=str)
+args = parser.parse_args()
+
+data = FileUtils(args.sourcefile, FileType.JSON).doRead()
+stop_words = FileUtils(args.stopwords, FileType.TEXT, ["words"]).doRead()
 
 #print data.iloc[0]['content']
 dict_word = {}
 theta = [0.3, 0.5, 0.7]
 all_words = []
 for i in range(len(data.index)):
-    doc = DataFactoryImpl(data.iloc[i:i+1], '../train/user.dict', stop_words).getAllWords()
+    doc = DataFactoryImpl(data.iloc[i:i+1], args.userdict, stop_words).getAllWords()
     all_words.append(doc)
     doc_word = list(set(doc['content'].tolist()[0]))
     X = LDAHelpers(doc, doc_word).getTFMat()
