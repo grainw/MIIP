257cb5f86757fecbfa4c4f336d975b544f05d7ec
liyu9 <liyu9@jd.com>
2017-01-05 11:09:38 +0800

    test

--- src/CommonUtils.py
+++ src/CommonUtils.py
@@ -11,8 +11,8 @@ import os
 log = Logger('milp.log', logging.INFO, logging.INFO)
 
 
-modelPath ='../data/testmodel/train_model'
+modelPath ='../data/testmodel/dxyallwith50'
 stopWordsPath ='../data/testmodel/stopwords1.txt'
-userDictPath ='../data/testmodel/user.dict'
-allWordsPath ='../data/testmodel/word_set'
+userDictPath ='../data/testmodel/user1.dict'
+allWordsPath ='../data/dxy/dxy50.csv'
 sourceFilePath ='../data/testmodel/dxy1.json'
--- src/ModelPredict.py
+++ src/ModelPredict.py
@@ -20,27 +20,37 @@ class ModelPredict:
         self.data_samples = None
         self.data_label = None
         self.stop_words = None
-        self.train_all_words = pickle.load(open(allWordsPath))
+        # self.train_all_words = pickle.load(open(allWordsPath))
+        self.train_all_words = None
         self.listdoc_words  = None
         self.zhuti = None
         self.docIdxs = None
+        self.predict_zhuti = []
         self.features = 15
         self.trainModel  = pickle.load(open(modelPath))
+        self.dictVail = []
+        self.num = 2
     '''
     input sourceFilePath
     output X matrix tf
     '''
     def processData(self):
         data_samples = FileUtils(self.sourceFilePath, FileType.JSON).doRead()
-        self.data_label = data_samples['label']
+        # self.data_label = data_samples['zhuti']
+
         self.zhuti = data_samples['zhuti']
-        stop_words = FileUtils(stopWordsPath, FileType.TEXT, ["stopwords"]).doRead()
+        self.stop_words = FileUtils(stopWordsPath, FileType.TEXT, ["stopwords"]).doRead()
+        # self.train_all_words =FileUtils(allWordsPath, FileType.CSV).doRead()['content']
+        self.train_all_words = DataFactoryImpl(FileUtils(allWordsPath, FileType.CSV).doRead(),self.stop_words).splitString()
         self.data_samples = data_samples
-        self.stop_words = stop_words
         list_models = []
         list_doc_words = []
-        for i in range(len(data_samples.index)):
-            doc = DataFactoryImpl(data_samples.iloc[i:i+1], stop_words,userDictPath).getAllWords()
+        for i in range(0,len(data_samples.index)/self.num):
+            doc = DataFactoryImpl(data_samples.iloc[i:i+1], self.stop_words,userDictPath).getAllWords()
+            log.info("doc"+str(doc))
+            if len(doc['content'].tolist()[0])==0:
+                self.dictVail.append(i)
+                continue
             doc_words =  list(set(doc['content'].tolist()[0]))
             list_doc_words.append(doc_words)
             X = LDAHelpers(doc, doc_words).getTFMat()
@@ -76,7 +86,7 @@ class ModelPredict:
         dist = []
         for tr in train:
             dist.append(la.norm(np.array(tr)-np.array(test)))
-        return np.array(dist).argsort()[0:-n_top_likely_topic-1:-1]
+        return np.array(dist).argsort()[0:n_top_likely_topic]
 
     def predict(self):
         #load model
@@ -89,22 +99,40 @@ class ModelPredict:
             train_topic_prob = self.getTrainTopWordsProb(self.trainModel.topic_word_, test_top_words, list(self.train_all_words))
             topicIdx = self.getMostLikelyTopic(train_topic_prob, test_topic_prob,self.n_top_likely_topic)
             log.info('most like docs:'+str(topicIdx))
-            for idx in topicIdx:
-                log.info(" ".join([list(self.train_all_words)[i]
-                                for i in self.trainModel.topic_word_[idx].argsort()[:-self.features- 1:-1]]))
+            # for idx in topicIdx:
+            #     log.info(" ".join([list(self.train_all_words)[i]
+            #                     for i in self.trainModel.topic_word_[idx].argsort()[:-self.features- 1:-1]]))
             for i in topicIdx:
+                # 只获取第一个主题
                 log.info(self.zhuti[self.trainModel.doc_topic_[:,i].argmax()])
-            for k in range(self.n_top_likely_topic):
-                if k ==0:
-                    docIdx = self.trainModel.doc_topic_[:,topicIdx[k]].argmax()
-                    docIdxs.append(docIdx)
-                    break
-        self.docIdxs = docIdxs
-        return docIdxs
+                self.predict_zhuti.append(self.zhuti[self.trainModel.doc_topic_[:,i].argmax()])
+                break
+        #     for k in range(self.n_top_likely_topic):
+        #         if k ==0:
+        #             docIdx = self.trainModel.doc_topic_[:,topicIdx[k]].argmax()
+        #             docIdxs.append(docIdx)
+        #             break
+        # self.docIdxs = docIdxs
+        # return docIdxs
 
     def accuray(self):
-        test_label = np.array(self.data_label).ravel()
-        train_label = np.array(self.docIdxs)
+        # test_label = np.array(self.data_label).ravel()
+        test_predict  = []
+        log.info("总数量为"+str(len(self.zhuti[0:len(self.data_samples.index)/self.num])))
+        log.info("过滤后总数为"+str(len(self.dictVail)))
+        log.info("过滤的序号为："+str(self.dictVail))
+        if len(self.dictVail)>0:
+            k = 0
+            for z in self.zhuti[0:len(self.data_samples.index)/self.num]:
+                if k not in self.dictVail:
+                    test_predict.append(z)
+                k = k+1
+            test_label =  np.array(test_predict).ravel()
+        else:
+            test_label = np.array( self.zhuti).ravel()
+        ##应用主题打标
+        # train_label = np.array(self.docIdxs)
+        train_label = np.array(self.predict_zhuti)
         result = (test_label == train_label)   # True则预测正确，False则预测错误
         c = np.count_nonzero(result)    # 统计预测正确的个数
         log.info(c)
