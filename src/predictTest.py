191220c55ba49374a40ee177feb88fbea6046a7d
liyu9 <liyu9@jd.com>
2017-02-21 14:47:03 +0800

    test MIIP model

--- src/predictTest.py
+++ src/predictTest.py
@@ -80,9 +80,11 @@ def getFetures(feturesData,label):
         rdata = edata[:pos]
         print '分割的数据为'+rdata
         if len(rdata)>0:
-            words = getAllWords(rdata)
+            #jieba分词
+            # words = getAllWords(rdata)
+            words = rdata.split("####")
             if len(words)>0:
-                labelF.append(words)
+                labelF.append([w for w in words if len(w)>0])
             else:
                 labelF.append([unicode(dict[int(label)], "utf-8") ])
             # print ' '.join(words)
@@ -91,7 +93,6 @@ def getFetures(feturesData,label):
             labelF.append(dict[int(label)])
 
     #保存模型
-    # pickle.load(open(allWordsPath))
     sp = saveModelPath+label+'fs'
     # os.mkdir(sp)
     saveModel = open( sp, 'w')
@@ -107,6 +108,13 @@ def getNWords(labelF,allwords):
             arr.append(i.encode("utf-8"))
     return list(set(allwords)),list(set(arr))
 
+
+def normalizationMatrix(x,Max,Min):
+    if Max == Min:
+        return 1
+    else:
+        return (x - Min) / (Max - Min);
+
 def DescribingSymptoms(label):
     raw_inputS = ''
     new_matrix = []
@@ -114,9 +122,7 @@ def DescribingSymptoms(label):
     featureList = cluster_list[int(label)][0]
     # labelF = getFetures(featureList,label)
     labelF = pickle.load(open(saveModelPath+label+'fs'))
-
     allwords = DataFactoryImpl(x_data, skc.stop_words,userDictPath).splitString()
-
     item = []
     for idx, s in x_data.iterrows():
         words = s['content'].strip().split(' ')
@@ -127,47 +133,82 @@ def DescribingSymptoms(label):
     x_matrix= LDAHelpers(x_data, allwords).getTFMat()
     # 将新的数据整理
     nWords,slabel = getNWords(labelF,allwords)
-    feautres = [j[0] for j in labelF]
-    print  '取相关特殊：'+'  '.join(feautres)
+    # feautres = [j[0] for j in labelF]
+    # print  '取相关特殊：'+'  '.join(feautres)
+    # for ss in slabel:
+    #     print ss+' '+str(6)
+    print ' '.join(list(set(slabel)))
     while True:
-        raw_inputS = raw_input('请输入您详细症状：')
-        #产生新的矩阵,对比矩阵
-        #拿到特征值组合成新的content
-        #todo拿状态值
-        new_words = getAllWords(raw_inputS)
-        print ','.join(new_words)
-        if len(new_words)>0:
-            setWord = set(new_words)
-            sWordSet = list(setWord)
-            new_matrix= getTF(new_words,sWordSet,labelF)
-            break
+        raw_inputS = raw_input('请输入您详细症状：').replace("\t","").replace(" ","").strip()
+        # new_words = getAllWords(raw_inputS)
+        # print ','.join(new_words)
+        # if len(new_words)>0:
+        #     setWord = set(new_words)
+        #     sWordSet = list(setWord)
+        #     new_matrix= getTF(new_words,sWordSet,labelF)
+        #     break
+        jieba.analyse.set_idf_path(idfPath)
+        jieba.analyse.set_stop_words(stopWordsPath)
+        tags = jieba.analyse.extract_tags(raw_inputS,topK=n_top_words,withWeight=True)#allowPOS=('n','ns','nsf','nt','nz','nl','ng')
+        dict = {}
+        keyWord = []
+        if len(tags)>0:
+            sorted(tags, key=lambda t: t[1])
+            min = tags[len(tags)-1][1]
+            max = tags[0][1]
+            dict = {}
+            for tag in tags:
+                dict[tag[0]] = normalizationMatrix(tag[1],max,min)
+                keyWord.append(tag[0])
+            flag = False
+            for kw in keyWord:
+                if kw in nWords:
+                    flag = True
+                    break
+            if flag:
+                break
 
-    new_model = lda.LDA(1, n_iter = 1500, random_state=1)
-    new_model.fit(np.array(new_matrix))
-
-    print '请等待........'
-
+    # jieba 代替lda生成向量矩阵
+    # new_model = lda.LDA(1, n_iter = 1500, random_state=1)
+    # new_model.fit(np.array(new_matrix))
     #转化为新的矩阵
     # avgValue = (np.array(x_matrix).argmax()- np.array(x_matrix).argmin())/2
     # n_matrix= LDAHelpers(x_data, nWords,slabel,avgValue).getTFMat()
     # train_model = lda.LDA(len(cluster_list[int(label)][0]), n_iter = 1500, random_state=1)
     # train_model.fit(np.array(n_matrix))
-
     train_model = pickle.load(open(saveModelPath+label+'model'))
     #savamodel
     # sp1 = saveModelPath+label+'model'
     # saveModel = open( sp1, 'w')
     # pickle.dump(train_model, saveModel)
-    # skc.printTopWords(train_model, nWords, 20,x_label=x_data)
-    test_topic_prob, test_top_words =getTestTopWords(new_model.topic_word_ ,sWordSet, n_top_words)
+    skc.printTopWords(train_model, nWords, 20,x_label=x_data)
+    # 组装成新的矩阵
+    test_top_words = []
+    test_topic_prob = []
+    for word in keyWord:
+        if word in nWords:
+            test_topic_prob.append(dict[word])
+            test_top_words.append(word)
+    # test_topic_prob, test_top_words =getTestTopWords(new_model.topic_word_ ,sWordSet, n_top_words)
     train_topic_prob = getTrainTopWordsProb(train_model.topic_word_, test_top_words, list(nWords))
     topicIdx = getMostLikelyTopic(train_topic_prob, test_topic_prob,n_top_likely_topic)
     print 'most like docs:'+str(topicIdx)
 
     print '预测结果：'
+    ids =[]
+    news_ids = []
     for i in topicIdx:
         # 只获取第一个主题
-        print x_data['zhuti'][train_model.doc_topic_[:,i].argmax()]
+        # print x_data['zhuti'][train_model.doc_topic_[:,i].argmax()]
+        ids.append(x_data['zhuti'][train_model.doc_topic_[:,i].argmax()])
+    for id in ids:
+        if id not in news_ids:
+            news_ids.append(id)
+    for i in news_ids:
+        print i
+
+
+
 
 
 while True:
@@ -175,8 +216,16 @@ while True:
     title = ''
     for i in dict:
         title += str(i)+":"+dict[i]+" "
-    print title
-    raw_inputF = raw_input('请选择您对应的选项标号：')
+    raw_inputF = ''
+    while True:
+        print title
+        raw_inputF = raw_input('请选择您对应的选项标号：').replace("\t","").replace(" ","").strip()
+        try:
+           if isinstance(int(raw_inputF),int):
+            break
+        except Exception as e:
+            print('输入数据格式有问题')
+
     print raw_inputF
     DescribingSymptoms(str(raw_inputF))
     continue
